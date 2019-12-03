2b2075f5423a61b0ef8de219b9a472086f4d1047
Katherine <kgn612@gmail.com>
2019-12-03 16:10:18 -0500

    Update Housing.py

--- Housing.py
+++ Housing.py
@@ -1,5 +1,7 @@
-from MainDateChecker import *
+from master import *
 
-# Runs function, checks if there's new, updated data, replaces old file with newer
-# Puts older data into ArchiveData
-choos_func('get_housing_update')
+connect_csv_files('PHL_haima_houses_damaged_pcoded_ndrrmc_sitrep_9_20161025.csv4.CSV','province','#adm2+name',
+r'/Users/katherinenewcomb/Desktop/Philippines/housingv2.csv','mun_code',
+r'/Users/katherinenewcomb/Desktop/TestingRepo/Philippines/ConnectFiles/YAY.csv',
+r'/Users/katherinenewcomb/Desktop/TestingRepo/Philippines/ConnectFiles/Gadm36_PHL_2.csv','CC_2',
+r'/Users/katherinenewcomb/Desktop/TestingRepo/Philippines/ConnectFiles/YAYv1.csv')
