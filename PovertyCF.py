664c20ec0a08df138323f628bea72cb8cc33164b
Katherine <kgn612@gmail.com>
2019-12-03 16:06:30 -0500

    Create PovertyCF.py

---
+++ PovertyCF.py
@@ -0,0 +1,3 @@
+from master import *
+
+connect_xlsx_files('190710_poverty-statistics.xlsx1.XLSX','poverty','/Users/katherinenewcomb/Desktop/Philippines/ConvertedFiles/poverty/{0}.xlsx','Sheet',csvpath,colname_removeRow,identifier,removed_row_outpath,colname_csv_edit,edited_csv_outpath,gadmFile,gadm_colname,merged_outpath)
